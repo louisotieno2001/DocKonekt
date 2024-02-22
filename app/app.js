38bc7dcded05606e7b22b902aa42e92a47b83b5d
LouisO <louis2001@gmail.com>
2024-02-22 11:18:13 +0300

    Whole website complete

--- app/app.js
+++ app/app.js
@@ -4,12 +4,17 @@ const bcrypt = require('bcrypt');
 const { Pool } = require('pg');
 const path = require('path');
 require('dotenv').config();
+const Fuse = require('fuse.js');
+const cors = require('cors');
+
+
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 const saltRounds = 10; // Number of salt rounds for bcrypt
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -20,7 +25,11 @@ const pool = new Pool({
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
-    ssl: { rejectUnauthorized: false } // Necessary for connecting to PostgreSQL on some platforms
+    ssl: { rejectUnauthorized: false } 
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.post('/register', async (req, res) => {
@@ -87,6 +96,8 @@ app.post('/messages', async (req, res) => {
         const result = await pool.query(query, values);
         const insertedRow = result.rows[0];
 
+        console.log(result);
+
         // Send a response indicating successful insertion
         res.status(201).json({
             message: 'Data inserted successfully',
@@ -95,6 +106,69 @@ app.post('/messages', async (req, res) => {
     } catch (error) {
         console.error('Error while inserting data:', error);
         res.status(500).json({ message: 'Internal server error', error: error.message });
+    }
+});
+
+app.get('/api/hospitals', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT hospital_name, hospital_url FROM hospitals');
+        const hospitals = result.rows;
+        res.json(hospitals);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+// Route for fuzzy hospital search
+app.post('/api/search-hospitals', async (req, res) => {
+    const { query } = req.body;
+
+    try {
+        const response = await fetch(`http://localhost:${port}/api/hospitals`);
+        const data = await response.json();
+
+        // Configure fuzzy search options
+        const fuseOptions = {
+            keys: ['hospital_name'],
+        };
+
+        // Create a new instance of Fuse with the options
+        const fuse = new Fuse(data, fuseOptions);
+
+        // Perform fuzzy search
+        const results = fuse.search(query);
+
+        // Send the fuzzy search results to the client
+        res.json(results);
+    } catch (error) {
+        console.error('Error searching hospitals:', error);
+        res.sendStatus(500);
+    }
+});
+
+app.get('/api/blog-content', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT title, body, time, date FROM blogs');
+
+        if (result.rows.length > 0) {
+            const blogContentList = result.rows.map(blogContent => ({
+                title: blogContent.title,
+                body: blogContent.body,
+                time: blogContent.time,
+                date: blogContent.date,
+            }));
+
+            res.json(blogContentList);
+
+            console.log(result);
+            
+        } else {
+            res.status(404).json({ error: 'No blog posts found' });
+        }
+    } catch (error) {
+        console.error('Error fetching blog content:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
     }
 });
 
