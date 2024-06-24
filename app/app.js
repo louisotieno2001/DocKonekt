ef75d1039b5fba91e89e8a02edd534e469dfa353
LouisO <louisotieno2001@gmail.com>
2024-06-24 16:03:02 +0300

    Pushing directus

--- app/app.js
+++ app/app.js
@@ -45,7 +45,7 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     cookie: {
-        maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+        maxAge: 30 * 24 * 60 * 60 * 1000,
     },
 }));
 
@@ -53,7 +53,7 @@ const checkSession = (req, res, next) => {
     if (req.session.user) {
         next(); // Continue to the next middleware or route
     } else {
-        res.redirect('/home'); // Redirect to the login page if no session is found
+        res.redirect('/login'); // Redirect to the login page if no session is found
     }
 };
 
@@ -286,7 +286,7 @@ app.post('/login', async (req, res) => {
             req.session.user = user;
             // Respond with success message and redirect URL for verified users
             return res.status(200).json({ message: 'Login successful', redirect: '/home' });
-        } else {
+        } else if(user.checked === false) {
             // Respond with redirect URL for unverified users
             return res.status(200).json({ message: 'Login successful', redirect: '/guideline' });
         }
