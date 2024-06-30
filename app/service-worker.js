f2eea401868045cd072a1ca3fb1bf65164016f6b
LouisO <louisotieno2001@gmail.com>
2024-06-30 15:18:18 +0300

    Implementing PWA

--- app/service-worker.js
+++ app/service-worker.js
@@ -1,26 +1,24 @@
 // Install the service worker
 self.addEventListener('install', function(event) {
-    event.waitUntil(
-      caches.open('your-app-cache').then(function(cache) {
-        return cache.addAll([
-          '/',
-          '/styles/index.css',
-          '/styles/small-devices.css',
-          'app.js',
-          '/res/Picture4.png',
-          '/res/Picture4.png'
-          // Add more URLs to cache as needed
-        ]);
-      })
-    );
-  });
-  
-  // Serve cached content when offline
-  self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.match(event.request).then(function(response) {
-        return response || fetch(event.request);
-      })
-    );
-  });
-  
+  event.waitUntil(
+    caches.open('your-app-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/styles/index.css',
+        '/styles/small-devices.css',
+        '/app.js',
+        '/res/Picture4.png'
+        // Add more URLs to cache as needed
+      ]);
+    })
+  );
+});
+
+// Serve cached content when offline
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
